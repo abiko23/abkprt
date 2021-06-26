5786842bf1782c32c274c7095831d73b6aa9c48f
abiko23 <62099230+abiko23@users.noreply.github.com>
2021-06-27 03:18:49 +0900

    first

---
+++ header.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+  <link rel="stylesheet" href="style.css" >
+  <?php wp_head(); ?>
+  <meta charset="<?php bloginfo( 'charset' ); ?>" />
+  <h1> This is my page </h1>
+  <div class='test header'>To je moi page </div>
+</head>
+
+<body>
+  <header>
+  </header>
---
+++ index.php
@@ -0,0 +1,2 @@
+<h1>Test index.php </h1>
+<h2> this is a test </h2>
